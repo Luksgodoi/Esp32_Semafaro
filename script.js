429281a0c1afa794e011fc6343baa73d1b153104
Luksgodoi <luksg@hotmail.com>
2025-07-23 10:48:25 -0300

    MQTT
    Implementado ao broker MQTT

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 
+let clienteweb =null
+
+const clienteId = "Esp32" + Math.floor(Math.random()*900) + 100
+// onfiguração para conectat ao broker MQTT o método Paho.MQTT.Cliente necessita do Host, número da Porta e o cliente ID
+clienteweb = new Paho.MQTT.Client("broker.hivemq.com",8884,clienteId)
+
+// Conectar ao MQTT
+clienteweb.connect({
+    useSSL:true, // utilizando a conexão segura para poder hospedar o render
+    onSuccess: function(){ // função para ao conectar ao broker informar que conectou com sucesso
+        alert("Conectado ao broker com sucesso :)")
+    },
+    onFailure: function(){ // em caso de falha, informa o erro
+        alert("A conexão falhou:(")
+    }
+})
 function ligarvermelho(){
 
     document.getElementById("vermelho").classList.add("vermelho")
